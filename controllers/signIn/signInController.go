59cb89b9c4f48de64ab91d7887f1d7bb13c202d8
石田大智 <ishidadaichi@ishidataichinoMacBook-Pro.local>
2023-07-10 00:32:12 +0900

    ログイン処理を追加

--- controllers/signIn/signInController.go
+++ controllers/signIn/signInController.go
@@ -2,9 +2,15 @@ package signIn
 
 import (
 	"GoRestAPI/ja"
-	"GoRestAPI/utils"
+	"GoRestAPI/models"
+	Utils "GoRestAPI/utils"
 	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 
@@ -19,4 +25,40 @@ func SignInHandler(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(errorMessage))
 		return
 	}
+
+	var reqBody models.LoginParams
+	body, error := io.ReadAll(r.Body)
+	if error != nil {		
+		w.WriteHeader(http.StatusInternalServerError)
+		errorMessage := Utils.ErrorMessage(ja.ServerErrorMessage)
+		w.Write([]byte(errorMessage))
+		return
+	}
+
+	if err := json.Unmarshal(body, &reqBody); err != nil {		
+		w.WriteHeader(http.StatusInternalServerError)
+		errorMessage := Utils.ErrorMessage(ja.EncodeErrorMessage)
+		w.Write([]byte(errorMessage))
+		return
+	}
+
+	response := db.QueryRow("SELECT password FROM User WHERE email = ? ", reqBody.Email)
+
+	var passwordDigest string
+	if scanError := response.Scan(&passwordDigest); scanError != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		errorMessage := Utils.ErrorMessage(ja.ServerErrorMessage)
+		w.Write([]byte(errorMessage))
+		return 
+	}
+
+	missingPasswordError := bcrypt.CompareHashAndPassword([]byte(passwordDigest), []byte(reqBody.Password))
+	if missingPasswordError != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		errorMessage := Utils.ErrorMessage(ja.PasswordMissingMessage)
+		w.Write([]byte(errorMessage))
+		return 
+	}
+
+	defer db.Close()
 }
